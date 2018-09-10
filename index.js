a20bf4a909021633196178f7ad59783418ba5363
Monique Hunte <monhunte@gmail.com>
2018-09-10 16:29:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var name = "Susan"
-var height = "70"
+var name = "Susan";
+var height = "70";
 
 if(name === "Susan"){
-  conosole.log("The name is correct")
+  conosole.log("The name is correct");
 } else {
-  console.log("Expected the name to be Susan")
+  console.log("Expected the name to be Susan");
 }
 
 // Don't worry about this
 module.exports = { name, height
-}
+};
 
